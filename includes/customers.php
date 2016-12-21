193fdc134b09017924445d3bd2f1c7f2338aa09f
Anne Minford <anne.minford@regus.com>
2016-12-21 15:53:49 +0000

    content updates

--- includes/customers.php
+++ includes/customers.php
@@ -3,32 +3,41 @@
 			<div id="wrapper">
 				<div id="carousel">
 					<div>
-						<a href="images/dog-1.png" title="Dog" data-hover="Sandy the west highland terrier" data-toggle="lightbox" class="lightbox">
-							<img src="images/dog-1.png" alt="Dog" />
+						<a href="images/ozzyAndLayla.jpg" title="Dog" data-hover="Ozzy &amp; Layla" data-toggle="lightbox" class="lightbox" data-title="Ozzy and Layla" data-footer="<p><span>Breed: </span> Border Terriers<br />
+						<span>Service: </span> Day Creche<br />
+						<span>Favourite game: </span> Tuggy</p>">
+							<img src="images/ozzyAndLayla.jpg" alt="Ozzy and Layla, Day Creche guests" />
 						</a>
 					</div>
 					<div>
-						<a href="images/dog-2.png" title="Dog" data-hover="Marty the yorkshire terrier" data-toggle="lightbox" class="lightbox">
+						<a href="images/dog-2.png" title="Dog" data-hover="PICTURE OF DOG" data-toggle="lightbox" class="lightbox" data-title="Title in here" data-footer="Brief description in here">
 							<img src="images/dog-2.png" alt="Dog" />
 						</a>
 					</div>
 					<div>
-						<a href="images/dog-3.png" title="Dog" data-hover="Kyla the bull terrier" data-toggle="lightbox" class="lightbox">
+						<a href="images/dog-3.png" title="Dog" data-hover="PICTURE OF DOG" data-toggle="lightbox" class="lightbox" data-title="Title in here" data-footer="Brief description in here">
 							<img src="images/dog-3.png" alt="Dog" />
 						</a>
 					</div>
 					<div>
-						<a href="images/dog-1.png" title="Dog" data-hover="Marty the yorkshire terrier" data-toggle="lightbox" class="lightbox">
+						<a href="images/ozzyAndLayla.jpg" title="Dog" data-hover="Ozzy &amp; Layla" data-toggle="lightbox" class="lightbox" data-title="Ozzy and Layla" data-footer="<p><span>Breed: </span> Border Terriers<br />
+						<span>Service: </span> Day Creche<br />
+						<span>Favourite game: </span> Fetch</p>">
+							<img src="images/ozzyAndLayla.jpg" alt="Ozzy and Layla, Day Creche guests" />
+						</a>
+					</div>
+					<div>
+						<a href="images/dog-1.png" title="Dog" data-hover="PICTURE OF DOG" data-toggle="lightbox" class="lightbox" data-title="Title in here" data-footer="Brief description in here">
 							<img src="images/dog-1.png" alt="Dog" />
 						</a>
 					</div>
 					<div>
-						<a href="images/dog-2.png" title="Dog" data-hover="Sandy the west highland terrier" data-toggle="lightbox" class="lightbox">
+						<a href="images/dog-2.png" title="Dog" data-hover="PICTURE OF KENNEL BLOCK" data-toggle="lightbox" class="lightbox" data-title="Title in here" data-footer="Brief description in here">
 							<img src="images/dog-2.png" alt="Dog" />
 						</a>
 					</div>
 					<div>
-						<a href="images/dog-3.png" title="Dog" data-hover="Kyla the bull terrier" data-toggle="lightbox" class="lightbox">
+						<a href="images/dog-3.png" title="Dog" data-hover="PICTURE OF KENNEL BLOCK" data-toggle="lightbox" class="lightbox data-title="Title in here" data-footer="Brief description in here">
 							<img src="images/dog-3.png" alt="Dog" />
 						</a>
 					</div>
